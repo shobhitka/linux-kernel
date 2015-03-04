5970de5d42d136e382b8c5689b6ff24a69d54774
Shobhit Kumar <shobhit.kumar@intel.com>
2015-03-04 13:24:22 +0530

    drivers/mfd: Add lookup table for Panel Control as GPIO signal
    On some Intel SoC platforms, the panel enable/disable signals are
    controlled by CRC PMIC. Add those control as a new GPIO in a lookup
    table for gpio-crystalcove chip during CRC driver load
    
    CC: Samuel Ortiz <sameo@linux.intel.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Alexandre Courbot <gnurou@gmail.com>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>

--- drivers/mfd/intel_soc_pmic_core.c
+++ drivers/mfd/intel_soc_pmic_core.c
@@ -24,7 +24,18 @@
 #include <linux/acpi.h>
 #include <linux/regmap.h>
 #include <linux/mfd/intel_soc_pmic.h>
+#include <linux/gpio/machine.h>
 #include "intel_soc_pmic_core.h"
+
+/* Lookup table for the Panel Enable/Disable line as GPIO signals */
+struct gpiod_lookup_table panel_gpio_table = {
+	/* Intel GFX is consumer */
+	.dev_id = "0000:00:02.0",
+	.table = {
+		/* Panel EN/DISABLE */
+		GPIO_LOOKUP("gpio_crystalcove", 94, "panel", GPIO_ACTIVE_HIGH),
+	},
+};
 
 /*
  * On some boards the PMIC interrupt may come from a GPIO line.
@@ -84,6 +95,9 @@ static int intel_soc_pmic_i2c_probe(struct i2c_client *i2c,
 	ret = enable_irq_wake(pmic->irq);
 	if (ret)
 		dev_warn(dev, "Can't enable IRQ as wake source: %d\n", ret);
+
+	/* Add lookup table binding for Panel Control to the GPIO Chip */
+	gpiod_add_lookup_table(&panel_gpio_table);
 
 	ret = mfd_add_devices(dev, -1, config->cell_dev,
 			      config->n_cell_devs, NULL, 0,
